9444fa4ec5378cef884399bc0c8cc60b4e60d8ec
David Rakosi <david.rakosi99@gmail.com>
2020-08-01 16:39:15 +0200

    cleanup and get started

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// import firebase from 'firebase'
+
+// const firebaseApp = new firebase.initializeApp(
+//     apiKey: "AIzaSyDd4vLhu94p3oNkbJsMoGPsvBj9yn3DanQ",
+//     authDomain: "clone-93835.firebaseapp.com",
+//     databaseURL: "https://clone-93835.firebaseio.com",
+//     projectId: "clone-93835",
+//     storageBucket: "clone-93835.appspot.com",
+//     messagingSenderId: "599508499081",
+//     appId: "1:599508499081:web:a5a34f646777e6f3d16458"
+// )
